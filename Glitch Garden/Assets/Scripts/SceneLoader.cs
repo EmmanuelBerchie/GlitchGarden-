cd02f516d72c552b9af95ad7bf78e7ab0f61fe97
Emmanuel Berchie <emmanuelberchie9@gmail.com>
2020-03-05 18:12:52 +0000

    Created Splash and Start scenes
    Also added audio and a coroutine to delay the wait for the start screen to show from the splash screen

---
+++ Glitch Garden/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+    [SerializeField] int timeToWait = 7;
+     void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if( currentSceneIndex == 0)
+        {
+            StartCoroutine(gameDelay()); 
+        }
+
+        
+    }
+   
+
+    public void LoadNextScene()
+    {
+     SceneManager.LoadScene(currentSceneIndex + 1); 
+    } 
+
+    IEnumerator gameDelay()
+    {
+      
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+
+}
+
